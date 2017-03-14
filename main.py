01ed4f0b43261918351ee346985fd84358c15b30
minnalkunnan <minnalkunnan@gmail.com>
2017-03-13 23:37:47 -0700

    Fixed main.py

--- main.py
+++ main.py
@@ -3,30 +3,26 @@ import mkRand
 
 def main():
     
-	
-	
 	j = 0
-	f = open('integer.out', 'w')
-	while j < 200:
-		val = 0
+	f = open('randomData.out', 'a')
+	while j < 50:
 		vals = []
-		#i = 0
-		#while i < 8:
-		twitchData = twitchScraper.collectData()
-		print(twitchData)
+		i = 0
+		while i < 8:
+		    twitchData = twitchScraper.collectData()
+		    print(twitchData)
 
-		randomVal = mkRand.get1bitlen(twitchData)
-		#print(randomVal)
-		#f.write(randomVal)
+		    randomVal = mkRand.get4bitlen(twitchData)
+		    print(randomVal)
 
-		vals.append(randomVal)
-		#i += 1
+		    vals.append(randomVal)
+		    i += 1
 
-		#print(vals)
-		#finalVal = mkRand.getInt(vals)
-		#print(finalVal)
+		print(vals)
+		finalVal = mkRand.getInt(vals)
+		print(finalVal)
 
-		f.write(str(val))
+		f.write(finalVal)
 		j += 1
 
 if __name__ == "__main__":
